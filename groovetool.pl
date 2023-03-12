365c9e68a432d8b2c36e6f24464f94dc59c28ad9
Gene Boggs <gene@ology.net>
2023-03-12 13:24:51 -0700

    WIP

--- groovetool.pl
+++ groovetool.pl
@@ -168,17 +168,15 @@ MIDI: &nbsp;
 <div class="collapse" id="drumSettings">
   <p></p>
   <div class="form-floating d-inline-flex align-items-center">
+    <input type="text" class="form-control form-control-sm" id="euclid" name="euclid" value="<%= $euclid %>" title="Space-separated kick,snare onset list" aria-describedby="euclidHelp">
+    <label for="euclid">Euclid:</label>
+    <small id="euclidHelp" class="form-text text-muted">&lt;kick_onsets&gt;,&lt;snare_onsets&gt;</small>
+  </div>
+  <div class="form-floating d-inline-flex align-items-center">
     <input type="number" class="form-control form-control-sm" id="eumax" name="eumax" min="0" max="16" value="<%= $eumax %>" title="number of random grooves to generate unless given a euclid list">
     <label for="eumax">Max:</label>
   </div>
-  <div class="form-floating d-inline-flex align-items-center">
-    <input type="text" class="form-control form-control-sm" id="euclid" name="euclid" value="<%= $euclid %>" title="Space-separated kick,snare onset list" aria-describedby="euclidHelp">
-    <label for="euclid">Euclid:</label>
-  </div>
-  <div>
-    <small id="euclidHelp" class="form-text text-muted">Form: &lt;kick_onsets&gt;,&lt;snare_onsets&gt;</small>
-    <p></p>
-  </div>
+  <p></p>
   <div class="form-floating d-inline-flex align-items-center">
     <input type="number" class="form-control form-control-sm" id="dvolume" name="dvolume" min="0" max="127" value="<%= $dvolume %>" title="0 to 127 defining the drums volume">
     <label for="dvolume">Volume:</label>
