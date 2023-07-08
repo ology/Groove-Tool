1b29ddeb08fc23b55096bb0f37e43b06d6bf40ed
Gene Boggs <gene@ology.net>
2023-07-08 00:16:20 -0700

    Don't die

--- groovetool.pl
+++ groovetool.pl
@@ -123,7 +123,7 @@ get '/' => sub ($c) {
     $mp3 = "public/$stamp.mp3";
     my $cmd = qq(timidity -c $ENV{HOME}/timidity.cfg $filename -Ow -o - | ffmpeg -i - -acodec libmp3lame -ab 64k $mp3);
     my ($stdout, $stderr, $exit) = capture { system($cmd) };
-    die 'Something went wrong' unless -e $mp3;
+#    die 'Something went wrong' unless -e $mp3;
     $mp3 =~ s/public//;
   }
 
