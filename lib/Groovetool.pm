7eb32b44253c8d7a9b7b37419e60ab9c73ca72a9
Gene Boggs <gene@ology.net>
2024-08-28 00:40:38 -0700

    Lose author libs

--- lib/Groovetool.pm
+++ lib/Groovetool.pm
@@ -1,6 +1,5 @@
 package Groovetool;
 
-use if $ENV{USER} eq "gene", lib => map { "$ENV{HOME}/sandbox/$_/lib" } qw(Data-Dataset-ChordProgressions Music-Bassline-Generator MIDI-Util MIDI-Drummer-Tiny Music-Duration Music-Duration-Partition);
 
 use Moo;
 use strictures 2;
